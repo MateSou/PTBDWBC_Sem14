21854b375494aee06e922b0d40ed6f7c11e84c27
Mateus <ma.jes1203@gmail.com>
2025-05-20 11:28:47 -0300

    Paginas e scrips js

---
+++ scripts/questao-03.js
@@ -0,0 +1,81 @@
+var alunos = new Array();
+var alunos = [];
+
+var nota1;
+var nota2;
+var nome;
+var soma;
+
+//Joao
+nome = "João da Silva";
+nota1 = 7;
+nota2 = 8.5;
+soma = nota1 + nota2;
+alunos[0] = document.getElementById("alun1");
+alunos[0].innerText = "Nome: " + nome + "\n" +
+                      "Nota 1: " + nota1 + "\n" +
+                      "Nota 2: " + nota2 + "\n" +
+                      "Multiplicação 1: " + nota1*0.6 + "\n" +
+                      "Multiplicação 2: " + nota2*0.4 + "\n" +
+                      "Soma: " + soma + "\n";
+
+//Aluno 1
+alunos[1] = document.getElementById("alun2");
+nome = "Lima Barreto";
+nota1 = 10;
+nota2 = 7.5;
+soma = nota1 + nota2; 
+alunos[1].innerText = "Nome: " + nome + "\n" +
+                      "Nota 1: " + nota1 + "\n" +
+                      "Nota 2: " + nota2 + "\n" +
+                      "Multiplicação 1: " + nota1*0.6 + "\n" +
+                      "Multiplicação 2: " + nota2*0.4 + "\n" +
+                      "Soma: " + soma + "\n";
+//Aluno 2
+alunos[2] = document.getElementById("alun3");
+nome =  "Mariana Costa";
+nota1 = 6;
+nota2 = 9.1;
+soma = nota1 + nota2;
+alunos[2].innerText = "Nome: " + nome + "\n" +
+                      "Nota 1: " + nota1 + "\n" +
+                      "Nota 2: " + nota2 + "\n" +
+                      "Multiplicação 1: " + nota1*0.6 + "\n" +
+                      "Multiplicação 2: " + nota2*0.4 + "\n" +
+                      "Soma: " + soma + "\n";
+
+//Aluno 3
+alunos[3] = document.getElementById("alun4");
+nome = "Fernando Pessoa";
+nota1 = 3;
+nota2 = 7.4;
+soma = nota1 + nota2;
+alunos[3].innerText = "Nome: " + nome + "\n" +
+                      "Nota 1: " + nota1 + "\n" +
+                      "Nota 2: " + nota2 + "\n" +
+                      "Multiplicação 1: " + nota1*0.6 + "\n" +
+                      "Multiplicação 2: " + nota2*0.4 + "\n" +
+                      "Soma: " + soma + "\n";
+
+alunos[4] = document.getElementById("alun5");
+nome = "Machado de Assis";
+nota1 = 8;
+nota2 = 5.6;
+soma = nota1 + nota2;
+alunos[4].innerText = "Nome " + nome + "\n" +
+                      "Nota 1: " + nota1 + "\n" +
+                      "Nota 2: " + nota2 + "\n" +
+                      "Multiplicação 1: " + nota1*0.6 + "\n" +
+                      "Multiplicação 2: " + nota2*0.4 + "\n" +
+                      "Soma: " + soma + "\n";
+
+
+
+
+
+
+
+
+
+
+
